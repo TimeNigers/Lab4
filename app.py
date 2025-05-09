7ab15c80977dbdfa02d7067e65268aad18141378
TimeNigers <oprlgp@gmail.com>
2025-05-10 00:07:58 +0300

    first cot

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Cloud Run!'
+    return 'Тут якийсь текст і це добре чи ні?'
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))  
